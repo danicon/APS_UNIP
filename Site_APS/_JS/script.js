ae152823fbea964c8ae9d338694ea5b285aacc18
Danicon <dancon.alferes@gmail.com>
2020-07-12 11:30:39 -0300

    commit full APS

---
+++ Site_APS/_JS/script.js
@@ -0,0 +1,38 @@
+// Transição de imagens com CSS e JavaScript 
+
+let time = 2000,
+    currentImageIndex = 0,
+    images = document
+                .querySelectorAll("#slider img")
+    max = images.length;
+
+function nextImage() {
+
+    images[currentImageIndex]
+        .classList.remove("selected")
+
+    currentImageIndex++
+    
+    if(currentImageIndex >= max)
+        currentImageIndex = 0
+    
+    images[currentImageIndex]
+        .classList.add("selected")
+}    
+
+function start() {
+    setInterval(() => {
+        // troca de image
+        nextImage()
+    }, time)
+}
+
+window.addEventListener("load", start)
+
+// Menu
+
+function mudaFoto (foto)
+{
+    document.getElementById("icone").src = foto;
+}
+
